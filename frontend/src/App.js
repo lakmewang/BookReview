733b764f2a110ce7c134cdbfb9c95346fe89764b
Lakmewan Gambheera Arachchi <155669037+lakmewang@users.noreply.github.com>
2024-11-29 03:01:33 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Reviews from './components/Reviews';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Book Reviews</h1>
+      <Reviews />
+    </div>
+  );
+}
+
+export default App;
